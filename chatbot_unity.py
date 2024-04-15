685bc22324793f6f31e69920d7de2907c144ebeb
specialist4u <specialist4u@gmail.com>
2024-04-15 18:26:24 +0500

    Update chatbot_unity.py
    spelling fixed

--- chatbot_unity.py
+++ chatbot_unity.py
@@ -21,7 +21,7 @@ image_logo = Image.open('pngegg.png')
 
 st.set_page_config(page_title="Chatbot", layout="centered", initial_sidebar_state="collapsed", menu_items=None,)
 
-instructions = "Please ask the user about a) location, b) plans to own the vehicle long term or short term, and c) use of vehicle for commuting or weekend driving, and d) are you leasing or financing, so that you can suggest appropriate finance and insurance products. Start the conversation by introducing yourself first."
+instructions = "Please ask the user about a) location, b) plans to own the vehicle long term or short term, and c) use of vehicle for commuting or weekend driving, and d) are you leasing or financing, so that you can suggest appropriate finance and insurance products. Start the conversation by introducing yourself first. Use the word vehicle instead of car"
 
 questions_instructions = ""
 
@@ -113,7 +113,7 @@ system_role = """
 
         Please ask customer terse questions to get this information if the information has been provided show in summarized form and if user asks for details then show the average cost to repair/replace with crime rate and road conditions. Using this information, please show the average monthly cost for these products, and also ask the customer if they are interested in knowing more about specific product offerings, mentioning the products based on customer's info, in the form of terse questions. If customer asks a question in response, reply from the information given in this prompt. Please feel free to ask questions to the customer to get the information needed to select the products.
     
-        Always stay in character as a  vehicle dealer assistant.Respond in the same language as request. If question is asked in chinese response in chinese. Be terse in your answers but with all relevant information in summarized form. Always sell the Pre-paid Maintenance Contract with all sales. Limit to only 3 products provided to customer in summarized form, display costs at first. User the work vehicle instead of car.
+        Always stay in character as a  vehicle dealer assistant.Respond in the same language as request. If question is asked in chinese response in chinese. Be terse in your answers but with all relevant information in summarized form. Always sell the Pre-paid Maintenance Contract with all sales. Limit to only 3 products provided to customer in summarized form, display costs at first. Use the word vehicle instead of car.
 
         """
 
